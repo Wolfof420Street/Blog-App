d37469c5543b0e70b5562dfdd67450f835f6a5c6
Wolfof420Street <billodida420@gmail.com>
2019-08-15 14:18:53 +0300

    saving a full Post

--- app/src/main/java/com/wolf/blogapp/Activities/AddPostActivity.java
+++ app/src/main/java/com/wolf/blogapp/Activities/AddPostActivity.java
@@ -12,27 +12,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.wolf.blogapp.Model.Blog;
 import com.wolf.blogapp.R;
 
-import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddPostActivity extends AppCompatActivity {
 
     private ImageButton mPostImage;
     private EditText mTitleEditText;
     private EditText mDesc;
+    private StorageReference mStorageReference;
     private Button postButton;
     private FirebaseUser mUser;
     private FirebaseAuth mAuth;
-    private DatabaseReference mPostDatabse;
+    private DatabaseReference mPostDatabase;
     private ProgressDialog mProgress;
     private Uri mImageUri;
     private static final int GALLERY_CODE =1;
@@ -49,7 +54,9 @@ public class AddPostActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
 
-        mPostDatabse = FirebaseDatabase.getInstance().getReference().child("Blog");
+        mStorageReference = FirebaseStorage.getInstance().getReference();
+
+        mPostDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
 
         mPostImage = findViewById(R.id.imageButton);
         mTitleEditText = findViewById(R.id.editText);
@@ -89,19 +96,42 @@ public class AddPostActivity extends AppCompatActivity {
         mProgress.setMessage("Posting To Blog...");
         mProgress.show();
 
-        String titleVal = mTitleEditText.getText().toString().trim();
-        String descVal = mDesc.getText().toString().trim();
+        final String titleVal = mTitleEditText.getText().toString().trim();
+        final String descVal = mDesc.getText().toString().trim();
 
-        if (!TextUtils.isEmpty(titleVal) && !TextUtils.isEmpty(descVal)) {
-        Blog blog = new Blog("Title", "Description", "imageUrl", "timestamp", "userId");
-        mPostDatabse.setValue(blog).addOnSuccessListener(new OnSuccessListener<Void>() {
+        if (!TextUtils.isEmpty(titleVal) && !TextUtils.isEmpty(descVal) && mImageUri != null) {
+       /* Blog blog = new Blog("Title", "Description", "imageUrl", "timestamp", "userId");
+        mPostDatabase.setValue(blog).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
                 Toast.makeText(getApplicationContext(), "blog posted", Toast.LENGTH_LONG).show();
-                mProgress.dismiss();
+                mProgress.dismiss();*/
+
+       StorageReference filePath = mStorageReference.child("Blog_images").child(mImageUri.getLastPathSegment());
+                filePath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Task<Uri> downloadUrl = taskSnapshot.getStorage().getDownloadUrl();
+
+                        DatabaseReference newPost = mPostDatabase.push();
+
+                        Map<String, String> dataToSave = new HashMap<>();
+
+                        dataToSave.put("title", titleVal);
+                        dataToSave.put("desc", descVal);
+                        dataToSave.put("image", downloadUrl.toString());
+                        dataToSave.put("timestamp", String.valueOf(java.lang.System.currentTimeMillis()));
+                        dataToSave.put("userId", mUser.getUid());
+
+                        newPost.setValue(dataToSave);
+
+
+                        mProgress.dismiss();
+                    }
+                });
 
             }
-        });
+
         }
     }
-}
+
