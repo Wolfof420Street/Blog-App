41ff1b7de5ca69338a8bdcb6fc67531d7487dd33
Wolfof420Street <billodida420@gmail.com>
2019-08-15 13:40:29 +0300

    setting up Image Upload

--- app/src/main/java/com/wolf/blogapp/Activities/AddPostActivity.java
+++ app/src/main/java/com/wolf/blogapp/Activities/AddPostActivity.java
@@ -1,20 +1,28 @@
 package com.wolf.blogapp.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.wolf.blogapp.Model.Blog;
 import com.wolf.blogapp.R;
+
+import java.net.URI;
 
 public class AddPostActivity extends AppCompatActivity {
 
@@ -26,6 +34,8 @@ public class AddPostActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference mPostDatabse;
     private ProgressDialog mProgress;
+    private Uri mImageUri;
+    private static final int GALLERY_CODE =1;
 
 
     @Override
@@ -46,12 +56,33 @@ public class AddPostActivity extends AppCompatActivity {
         mDesc = findViewById(R.id.editText2);
         postButton = findViewById(R.id.submitPost);
 
+
+        mPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_CODE);
+            }
+        });
+
         postButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             startPosting();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            mImageUri = data.getData();
+
+            mPostImage.setImageURI(mImageUri);
+        }
     }
 
     private void startPosting() {
@@ -62,7 +93,15 @@ public class AddPostActivity extends AppCompatActivity {
         String descVal = mDesc.getText().toString().trim();
 
         if (!TextUtils.isEmpty(titleVal) && !TextUtils.isEmpty(descVal)) {
+        Blog blog = new Blog("Title", "Description", "imageUrl", "timestamp", "userId");
+        mPostDatabse.setValue(blog).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(), "blog posted", Toast.LENGTH_LONG).show();
+                mProgress.dismiss();
 
+            }
+        });
         }
     }
 }
