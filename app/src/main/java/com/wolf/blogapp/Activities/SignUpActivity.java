0b2472648e5fed5246fc2197e989a83e50c1b5f1
Wolfof420Street <billodida420@gmail.com>
2019-08-15 17:06:33 +0300

    adding an avatar

--- app/src/main/java/com/wolf/blogapp/Activities/SignUpActivity.java
+++ app/src/main/java/com/wolf/blogapp/Activities/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.wolf.blogapp.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -9,6 +10,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
@@ -23,12 +25,13 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText secondName;
     private EditText email;
     private EditText password;
+    private ImageButton profilePic;
     private Button createAcctButton;
     private DatabaseReference mDatabaseReference;
     private FirebaseDatabase mFireBaseDatabase;
     private FirebaseAuth mAuth;
     private ProgressDialog progressDialog;
-
+    private static final int GALLERY_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +49,17 @@ public class SignUpActivity extends AppCompatActivity {
         email = findViewById(R.id.emailText);
         password = findViewById(R.id.passwordText);
         createAcctButton = findViewById(R.id.submitButton);
+        profilePic = findViewById(R.id.profilePicture);
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_CODE);
+            }
+        });
 
         createAcctButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,6 +97,15 @@ public class SignUpActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode ==  GALLERY_CODE && requestCode == RESULT_OK) {
+
         }
     }
 }
